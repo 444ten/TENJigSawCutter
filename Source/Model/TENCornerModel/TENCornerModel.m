b15f5fe6507eb420b671f0d74501146bccb00782
444ten <444ten@gmail.com>
2016-08-08 14:55:46 +0300

    TENCornerModel -> initial commit

---
+++ Source/Model/TENCornerModel/TENCornerModel.m
@@ -0,0 +1,13 @@
+//
+//  TENCornerModel.m
+//  TENJigSawCutter
+//
+//  Created by 444ten on 8/8/16.
+//  Copyright © 2016 444ten. All rights reserved.
+//
+
+#import "TENCornerModel.h"
+
+@implementation TENCornerModel
+
+@end
