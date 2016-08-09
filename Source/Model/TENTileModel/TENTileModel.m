42fb56b5d62f478a897d2aceae57c7362f459678
444ten <444ten@gmail.com>
2016-08-09 08:11:29 +0300

    TENTileModel -> initial commit

---
+++ Source/Model/TENTileModel/TENTileModel.m
@@ -0,0 +1,13 @@
+//
+//  TENTileModel.m
+//  TENJigSawCutter
+//
+//  Created by 444ten on 8/9/16.
+//  Copyright © 2016 444ten. All rights reserved.
+//
+
+#import "TENTileModel.h"
+
+@implementation TENTileModel
+
+@end
