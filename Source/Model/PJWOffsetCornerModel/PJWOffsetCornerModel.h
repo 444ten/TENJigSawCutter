2cf9fba6f390b5716230afec785d494dd623376e
444ten <444ten@gmail.com>
2016-08-10 11:51:41 +0300

    PJWOffsetCornerModel -> initial commit

---
+++ Source/Model/PJWOffsetCornerModel/PJWOffsetCornerModel.h
@@ -0,0 +1,13 @@
+//
+//  PJWOffsetCornerModel.h
+//  TENJigSawCutter
+//
+//  Created by 444ten on 8/10/16.
+//  Copyright © 2016 444ten. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface PJWOffsetCornerModel : NSObject
+
+@end
---
+++ Source/Model/PJWOffsetCornerModel/PJWOffsetCornerModel.m
@@ -0,0 +1,13 @@
+//
+//  PJWOffsetCornerModel.m
+//  TENJigSawCutter
+//
+//  Created by 444ten on 8/10/16.
+//  Copyright © 2016 444ten. All rights reserved.
+//
+
+#import "PJWOffsetCornerModel.h"
+
+@implementation PJWOffsetCornerModel
+
+@end
