11edc62e4c6fbad7b4b9c46a3a7caea5243df964
444ten <444ten@gmail.com>
2016-08-11 11:54:02 +0300

    JPWTileImageView -> initial commit

---
+++ Source/UI/JPWTileImageView/JPWTileImageView.h
@@ -0,0 +1,13 @@
+//
+//  JPWTileImageView.h
+//  TENJigSawCutter
+//
+//  Created by 444ten on 8/11/16.
+//  Copyright © 2016 444ten. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface JPWTileImageView : UIImageView
+
+@end
---
+++ Source/UI/JPWTileImageView/JPWTileImageView.m
@@ -0,0 +1,21 @@
+//
+//  JPWTileImageView.m
+//  TENJigSawCutter
+//
+//  Created by 444ten on 8/11/16.
+//  Copyright © 2016 444ten. All rights reserved.
+//
+
+#import "JPWTileImageView.h"
+
+@implementation JPWTileImageView
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
