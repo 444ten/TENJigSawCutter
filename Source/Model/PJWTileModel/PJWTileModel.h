af8f33ebd347d4552ec4966302e258215de1290e
444ten <444ten@gmail.com>
2016-08-15 09:43:11 +0300

    PJWTileModel -> initial commit

---
+++ Source/Model/PJWTileModel/PJWTileModel.h
@@ -0,0 +1,13 @@
+//
+//  PJWTileModel.h
+//  TENJigSawCutter
+//
+//  Created by 444ten on 8/15/16.
+//  Copyright © 2016 444ten. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface PJWTileModel : NSObject
+
+@end
