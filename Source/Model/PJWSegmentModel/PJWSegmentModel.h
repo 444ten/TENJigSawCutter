58f4d76aac94f3e0e77b34da58d7439b99b73602
444ten <444ten@gmail.com>
2016-08-19 15:48:11 +0300

    PJWSegmentModel -> initial commit

---
+++ Source/Model/PJWSegmentModel/PJWSegmentModel.h
@@ -0,0 +1,13 @@
+//
+//  PJWSegmentModel.h
+//  TENJigSawCutter
+//
+//  Created by 444ten on 8/19/16.
+//  Copyright © 2016 444ten. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface PJWSegmentModel : NSObject
+
+@end
---
+++ Source/Model/PJWSegmentModel/PJWSegmentModel.m
@@ -0,0 +1,13 @@
+//
+//  PJWSegmentModel.m
+//  TENJigSawCutter
+//
+//  Created by 444ten on 8/19/16.
+//  Copyright © 2016 444ten. All rights reserved.
+//
+
+#import "PJWSegmentModel.h"
+
+@implementation PJWSegmentModel
+
+@end
