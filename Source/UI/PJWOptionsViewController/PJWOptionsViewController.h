ad396caa8568b2540b23b93e9dafb394dea14ac1
444ten <444ten@gmail.com>
2016-08-23 14:29:56 +0300

    PJWOptionsViewController -> initial commit

---
+++ Source/UI/PJWOptionsViewController/PJWOptionsViewController.h
@@ -0,0 +1,13 @@
+//
+//  PJWOptionsViewController.h
+//  TENJigSawCutter
+//
+//  Created by 444ten on 8/23/16.
+//  Copyright © 2016 444ten. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface PJWOptionsViewController : UIViewController
+
+@end
---
+++ Source/UI/PJWOptionsViewController/PJWOptionsViewController.m
@@ -0,0 +1,37 @@
+//
+//  PJWOptionsViewController.m
+//  TENJigSawCutter
+//
+//  Created by 444ten on 8/23/16.
+//  Copyright © 2016 444ten. All rights reserved.
+//
+
+#import "PJWOptionsViewController.h"
+
+@interface PJWOptionsViewController ()
+
+@end
+
+@implementation PJWOptionsViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view from its nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
